0f1a6a67e50fe13f59ede47dd78e8f0cd78e91a3
aaronachang <aaron.chang@utexas.edu>
2016-10-11 15:45:37 -0500

    starter code

---
+++ src/assignment4/Algae.java
@@ -0,0 +1,27 @@
+/* CRITTERS Algae.java
+ * EE422C Project 4 submission by
+ * Aaron Chang
+ * AAC3434
+ * 16475
+ * Siva Manda
+ * SM48525
+ * 16480
+ * Slip days used: <0>
+ * Fall 2016
+ */
+package assignment4;
+/*
+ * Do not change this file.
+ */
+import assignment4.Critter.TestCritter;
+
+public class Algae extends TestCritter {
+
+	public String toString() { return "@"; }
+	
+	public boolean fight(String not_used) { return false; }
+	
+	public void doTimeStep() {
+		setEnergy(getEnergy() + Params.photosynthesis_energy_amount);
+	}
+}
---
+++ src/assignment4/InvalidCritterException.java
@@ -0,0 +1,25 @@
+/* CRITTERS InvalidCritterException.java
+ * EE422C Project 4 submission by
+ * Aaron Chang
+ * AAC3434
+ * 16475
+ * Siva Manda
+ * SM48525
+ * 16480
+ * Slip days used: <0>
+ * Fall 2016
+ */
+package assignment4;
+
+public class InvalidCritterException extends Exception {
+	String offending_class;
+	
+	public InvalidCritterException(String critter_class_name) {
+		offending_class = critter_class_name;
+	}
+	
+	public String toString() {
+		return "Invalid Critter Class: " + offending_class;
+	}
+
+}
