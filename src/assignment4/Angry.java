7939ce97a31fc0b6e45cfdb19a8d7d7f57ed904f
smanda7 <sivamanda@gmail.com>
2016-10-18 16:15:13 -0500

    Added code for methods

--- src/assignment4/Angry.java
+++ src/assignment4/Angry.java
@@ -20,14 +20,15 @@ public class Angry extends Critter {
 
 	@Override
 	public void doTimeStep() {
-		// TODO Auto-generated method stub
-		
+		//Angry walks around looking to fight
+		walk(Critter.getRandomInt(10));
+		Angry child = new Angry();
+		reproduce(child, getRandomInt(5));		
 	}
 
 	@Override
 	public boolean fight(String oponent) {
-		// TODO Auto-generated method stub
-		return false;
+		//Angry likes to fight all the time
+		return true;	
 	}
-
 }
