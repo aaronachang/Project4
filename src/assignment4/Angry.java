ccdd3f131dce9c0926a5e56c868a58c7ea1bb08c
Siva Manda <sivamanda@gmail.com>
2016-10-17 20:04:12 -0500

    My Critter

---
+++ src/assignment4/Angry.java
@@ -0,0 +1,21 @@
+/* CRITTERS InvalidCritterException.java
+ * EE422C Project 4 submission by
+ * Aaron Chang
+ * AAC3434
+ * 16475
+ * Siva Manda
+ * SM48525
+ * 16480
+ * Slip days used: <0>
+ * Fall 2016
+ */
+package assignment4;
+
+public class Angry extends Critter {
+	
+	@Override
+	public String toString(){
+		return "A";
+	}
+
+}
